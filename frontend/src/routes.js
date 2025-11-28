856bdd5853761f32dbbdf0b04e23fb1d9d75b175
evelllyn <tinghsin027@gmail.com>
2025-11-28 15:34:13 +0800

    set router base

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -37,7 +37,7 @@ const routes = [
 
 const router = createRouter({
   routes,
-  history: createWebHistory('/joodoo/')
+  history: createWebHistory(import.meta.env.BASE_URL)
 })
 
 router.beforeEach(to => {
